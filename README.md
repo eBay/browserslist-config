5d7c3300440728ac559aa9bb9cef6abc6a9cfe06
Sean Gates <sgates@ebay.com>
2018-04-09 16:44:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# browserlist-config-ebay
+
+This package provides Browserlist rules as a shared NPM module.
+
+## Usage
+
+```sh
+yarn add browserlist-config-ebay
+```
+
+or
+
+```sh
+npm install browserlist-config-ebay
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = [
+    "Chrome >= 49",
+    "Firefox >= 48",
+    "Safari >= 6",
+    "Edge >= 12",
+    "IE >= 9",
+    "Opera 47-48",
+    "ChromeAndroid >= 38",
+    "ios_saf >= 7",
+    "ie_mob >= 10",
+    "Android >= 3"
+];
