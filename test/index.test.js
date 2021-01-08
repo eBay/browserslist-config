e28d7d590a36843f41223a7bdf1dff74f8012c59
abiyasa <ilham.suhardi@gmail.com>
2021-01-08 16:15:33 +0100

    Add test
    install jest as dev dependency

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+const browserslist = require('browserslist');
+const config = require('../');
+
+it('should export an array', () => {
+    expect(Array.isArray(config)).toBe(true);
+});
+
+it('should not contain invalid queries', () => {
+    const result = browserslist(config);
+    expect(result).toBeTruthy();
+});
