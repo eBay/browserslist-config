6cfa0faeaaa34281d6a595c4690541aacdef7f5a
Abiyasa Suhardi <ilham.suhardi@gmail.com>
2021-01-27 16:03:29 +0100

    Update browsers version & add test (#5)
    * Update installation doc
    
    * Update the browser versions
    
    based on min version of Tier 2 browsers
    
    * Update the browser versions
    
    * Add test
    
    install jest as dev dependency
    
    * Add lint script
    
    * Ignore test file from npm publish

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = [
     'defaults',
-    'Chrome >= 49',
-    'Firefox >= 48',
-    'Safari >= 9',
-    'Edge >= 12',
+    'Chrome >= 70',
+    'Firefox >= 70',
+    'Safari >= 10',
+    'Edge >= 18',
     'IE >= 11',
-    'Opera 47-48',
-    'ChromeAndroid >= 38',
+    'Opera >= 72',
+    'ChromeAndroid >= 68',
     'ios_saf >= 9',
-    'Android >= 3',
+    'Android >= 4',
     'not dead'
 ];
