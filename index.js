0a7df835a87915b5f298cdd57868a78bf6bddbfe
Abiyasa Suhardi <ilham.suhardi@gmail.com>
2021-05-06 18:50:57 +0200

    Remove IE11 explicitly (#13)
    * Remove defaults from browserlists to exclude IE11
    * Fix the Edge versions to only allow Edge 18 and 87+

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 module.exports = [
-    'defaults',
     'Chrome >= 78',
     'Firefox >= 78',
     'Safari >= 10',
-    'Edge >= 18',
+    'Edge 18',
+    'Edge >= 87',
     'Opera >= 73',
     'ChromeAndroid >= 75',
     'ios_saf >= 9',
-    'Android >= 4',
-    'not dead'
+    'Android >= 4'
 ];
