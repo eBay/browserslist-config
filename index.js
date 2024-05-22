e4980c2c9d99e4d30294139b81ba18e391239d78
Mark Stuart <marstuart@paypal.com>
2024-05-22 01:38:33 -0700

    Update with May 2024 revision (#39)
    Update browser compatibility

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = [
-    'Chrome >= 93',
+    'Chrome >= 103',
     'Firefox >= 78',
     'Safari >= 12',
     'Edge >= 109',
     'Opera >= 95',
     'ChromeAndroid >= 106',
-    'ios_saf >= 12'
+    'ios_saf >= 14'
 ];
