28ad10526826e5086f4c8f06ca07cdada2a4aa4e
abiyasa <ilham.suhardi@gmail.com>
2025-12-16 21:53:12 +0100

    Update to reflect changes of Browser Policy Q4 2025

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = [
     'Chrome >= 109',
     'Firefox >= 115',
-    'Safari >= 14',
-    'Edge >= 109',
+    'Safari >= 15.4',
+    'Edge >= 140',
     'Opera >= 95',
     'ChromeAndroid >= 130',
-    'ios_saf >= 15'
+    'ios_saf >= 15.4'
 ];
